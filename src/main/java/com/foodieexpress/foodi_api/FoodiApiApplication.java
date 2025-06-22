c3aad04a635587fe15988f16219641946fe419cf
prabhanshu451 <prabhanshuu45@gmail.com>
2025-06-22 18:40:11 +0530

    Day 1

---
+++ src/main/java/com/foodieexpress/foodi_api/FoodiApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodieexpress.foodi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodieexpress/foodi_api/FoodiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodieexpress.foodi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
